f6407d4524ae80c3dac26c505a660e8ef6fbbe2e
Leo <lion24161582@gmail.com>
2023-10-13 01:36:48 +0800

    link list

---
+++ linklist.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+// linklist 
+typedef struct listNode *listPointer ; 
+typedef struct listNode
+{
+    int data;
+    listPointer next;
+}listNode;
+listPointer first = NULL ; 
+// insert to node 
+
+
+
+// delete node 
+
+
+
+
+// print nodes 
+
+
+
+
+
+int main() {
+
+
+    return 0 ; 
+}
