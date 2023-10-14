52c66a592d3eb03c01701db1c6b8b72ce17f0fc3
Leo <lion24161582@gmail.com>
2023-10-14 17:25:49 +0800

    linklist and liskstack

--- linklist.c
+++ linklist.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 // linklist 
 typedef struct listNode *listPointer ; 
 typedef struct listNode
@@ -7,23 +8,74 @@ typedef struct listNode
     listPointer next;
 }listNode;
 listPointer first = NULL ; 
-// insert to node 
+/**
+ * 函式描述：插入到節點x後面
+ * @param first ：指標指向該節點的第一個指標
+ * @param x ：節點x
+ * @param data：插入內容
+ * @return 
+ */
+void insert(listPointer *first, listPointer x,  int data )
+{
+    // 1. assign new spaces
+    listPointer new_node ;
+    new_node =  malloc(sizeof(*new_node)); 
+    new_node->data = data ;
 
+    if(*first)   // *first sotre address is not empty 
+    { 
+      new_node->next = x->next ;
+      x->next = new_node ;
+    }else// empty 
+    {
+        *first = new_node ;
+        new_node->next = NULL ;
+    }
 
-
-// delete node 
-
-
-
-
-// print nodes 
-
-
-
-
-
+}
+/**
+ * 函式描述：刪除節點x
+ * @param first ：first指向該第一個節點的指標
+ * @param trail ：你希望刪除的前一個節點
+ * @param x ：你希望刪除的節點
+ * @return 回傳值描述
+ */
+void delete(listPointer *first, listPointer trail , listPointer x)
+{
+    if(trail)
+    {
+        trail->next = x->next;
+    }else  // delete first if trail is null 
+    {
+        *first = (*first)->next ; // *first the first which means store the value of first element . 
+                                  // (*first)->next :the address of element saved 
+    }
+    free(x); 
+}
+/**
+ * 函式描述：顯示LinkList
+ * @param first ：first指向該第一個節點的指標
+ * @return 
+ */void printList(listPointer first)
+{
+    listPointer current = first ;
+    for(; current; current = current->next)
+    {
+        printf("%d ", current->data) ; 
+    }
+    printf("\n");
+}
 int main() {
-
+    // 插入節點
+    insert(&first, NULL, 10) ;
+    insert(&first, first, 30) ;
+    insert(&first, first, 40) ;
+    printList(first); 
+    delete(&first,NULL,first);     // 刪除第一個節點
+    printList(first);
+    //插入第一個節點 
+    insert(&first ,NULL,100);
+    printList(first);
 
     return 0 ; 
 }
